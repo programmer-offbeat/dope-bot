eb1a993607cd58e6c506ab180470147dff93cdbd
programmer-offbeat <87474641+programmer-offbeat@users.noreply.github.com>
2021-12-28 20:13:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import discord,os
 from dotenv import load_dotenv
-import jishaku
+from jishaku.cog import Jishaku
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='e!')
 
 @client.event()
 async def on_ready():
-  print(f"imded")
+  print("imded")
 
 client.load_extension('jishaku')
 client.run(os.getenv('TOKEN'))
